455471b8b206d1ec048a0dc74fb17d4fbaa2f0e5
vitalii <sokilvitaliy01@gmail.com>
2022-04-16 13:01:04 +0300

    fix

--- hello-world/vue.config.js
+++ hello-world/vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   css: {
     loaderOptions: {
       scss: {
-        additionalData: `@import "~@/assets/scss/app.scss";`
+        // additionalData: `@import "~@/assets/scss/app.scss";`
       },
     }
   }
